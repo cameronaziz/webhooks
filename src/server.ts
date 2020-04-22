c8335093d7aa70fade480b4ed772019253a60bc1
cameronaziz <cameronaziz@me.com>
2020-04-22 17:41:02 -0400

    remove node_modules

--- src/server.ts
+++ src/server.ts
@@ -1,13 +1,20 @@
 import * as express from "express";
+import * as serverless from "serverless-http";
 
 const app = express();
 
-app.get("/", (req, res) => {
+const router = express.Router();
+
+router.get("/", (req, res) => {
   res.send("Hello World");
 });
+
+app.use("/.netlify/function/server", router);
 
 const PORT = process.env.PORT || 3000;
 
 app.listen(PORT, () => {
   console.log(`Server is running in http://localhost:${PORT}`);
 });
+
+export const handler = serverless(app);
